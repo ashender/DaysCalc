f57d0ef74faab66e0e4d6359905165fca5b4d7ac
Andrei Shender <a.shender@viber.com>
2017-04-28 10:14:16 +0300

    Initial commit, project template

---
+++ DaysCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DaysCalc
+//
+//  Created by andrei shender on 4/28/17.
+//  Copyright © 2017 MoveForward. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
